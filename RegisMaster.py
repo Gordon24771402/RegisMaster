7e78b154f48157cc2577b928f0eecc98b5ed3152
Gordon24771402 <gordon24771402@gmail.com>
2020-05-23 14:20:49 +0800

    Error002 Fixed

--- RegisMaster.py
+++ RegisMaster.py
@@ -10,9 +10,11 @@ from playsound import playsound
 import time
 import random
 
+
 def os_mitmproxy():
-    os.chdir('C:/Users/Hao Kang')
-    os.system('mitmdump --mode upstream:127.0.0.1:7890 -s modify_response.py')
+    os.chdir('C:/Users/97262/PycharmProjects/RegisMaster')
+    os.system('mitmdump --mode upstream:127.0.0.1:7890 -s Modify_Response.py')
+
 
 def refresh_driver(driver):
     driver.refresh()
@@ -20,16 +22,19 @@ def refresh_driver(driver):
     driver.refresh()
     time.sleep(random.randint(3, 8))
 
+
 def reboot_main_program(driver):
     driver.close()
-    time.sleep(random.randint(300, 350))
+    time.sleep(random.randint(600, 1200))
     main_program()
+
 
 def error001(driver):
     html = str(driver.execute_script("return document.documentElement.outerHTML"))
     if "We are unable to process your request due to a system error." and "Please try again later." in html:
         refresh_driver(driver)
-        again = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[1]/div[1]/div/div/ul/li/a")))
+        again = WebDriverWait(driver, 120).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[1]/div[1]/div/div/ul/li/a")))
         again.click()
         time.sleep(random.randint(5, 15))
         try:
@@ -43,8 +48,26 @@ def error001(driver):
         except:
             reboot_main_program(driver)
 
+
+def error002(driver):
+    html = str(driver.execute_script("return document.documentElement.outerHTML"))
+    if "Please sign in." and "Welcome back." in html:
+        try:
+            iteration(error2_login(driver), driver)
+            iteration(actionRegisterAnother(driver), driver)
+            iteration(authenticatePage(driver), driver)
+            iteration(Continue001(driver), driver)
+            iteration(updateLater(driver), driver)
+            iteration(agreeTerms(driver), driver)
+            iteration(Continue002(driver), driver)
+            iteration(inspect_future_SAT(driver), driver)
+        except:
+            reboot_main_program(driver)
+
+
 def iteration(program, driver):
     try:
+        time.sleep(5)
         program
     except:
         try:
@@ -55,11 +78,16 @@ def iteration(program, driver):
             try:
                 error001(driver)
             except:
-                reboot_main_program(driver)
+                try:
+                    error002(driver)
+                except:
+                    reboot_main_program(driver)
+
 
 def get_collegeboard_website(driver):
     driver.get('https://collegereadiness.collegeboard.org/')
     time.sleep(random.randint(5, 15))
+
 
 def login(driver):
     sign_in = WebDriverWait(driver, 120).until(
@@ -67,41 +95,60 @@ def login(driver):
     sign_in.click()
     user_name = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,
                                                                              "/html/body/div[1]/div[1]/div/div/div[2]/div[3]/div/div/div/div/div/div[1]/div/div[2]/form/div[1]/input")))
-    user_name.send_keys("USERNAME")
+    user_name.send_keys("GORDON24771402")
     password = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,
                                                                             "/html/body/div[1]/div[1]/div/div/div[2]/div[3]/div/div/div/div/div/div[1]/div/div[2]/form/div[2]/input")))
-    password.send_keys("PASSWORD")
+    password.send_keys("Up5G2R_9VrN2-Kw")
     submit = WebDriverWait(driver, 120).until(EC.element_to_be_clickable(
         (By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]/div[3]/div/div/div/div/div/div[1]/div/div[2]/form/button")))
+    submit.click()
+    time.sleep(random.randint(5, 15))
+
+def error2_login(driver):
+    user_name = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,
+                                                                             "/html/body/div/div[3]/div/div/div[1]/div/div/div[2]/div/div/div/div/form/div[1]/div[1]/div[2]/input")))
+    user_name.send_keys("GORDON24771402")
+    password = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,
+                                                                            "/html/body/div/div[3]/div/div/div[1]/div/div/div[2]/div/div/div/div/form/div[1]/div[2]/div[2]/input")))
+    password.send_keys("Up5G2R_9VrN2-Kw")
+    submit = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.XPATH,
+                                                                          "/html/body/div/div[3]/div/div/div[1]/div/div/div[2]/div/div/div/div/form/div[3]/div[1]/button")))
     submit.click()
     time.sleep(random.randint(5, 15))
 
 def get_collegeboard_registration_website(driver):
     driver.get("https://nsat.collegeboard.org/satweb/satHomeAction.action")
 
+
 def actionRegisterAnother(driver):
     register_another = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "actionRegisterAnother")))
     register_another.click()
+
 
 def authenticatePage(driver):
     authenticate_page = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "authenticatePage")))
     authenticate_page.click()
 
+
 def Continue001(driver):
     continue001 = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "continue")))
     continue001.click()
+
 
 def updateLater(driver):
     update_later = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "updateLater")))
     update_later.click()
 
+
 def agreeTerms(driver):
     agree_terms = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "agreeTerms")))
     agree_terms.click()
 
+
 def Continue002(driver):
     continue002 = WebDriverWait(driver, 120).until(EC.element_to_be_clickable((By.ID, "continue")))
     continue002.click()
+
 
 def inspect_future_SAT(driver):
     while True:
@@ -113,14 +160,15 @@ def inspect_future_SAT(driver):
         elif "There are no available registration dates for the current test year. Please check back later to register for future tests." not in html:
             while True:
                 playsound("Alarm01.wav")
+
         time.sleep(random.randint(300, 350))
         driver.back()
         refresh_driver(driver)
-        iteration(agreeTerms, driver)
+        iteration(agreeTerms(driver), driver)
         iteration(Continue002(driver), driver)
 
+
 def main_program():
-    os.chdir('C:/Users/Hao Kang/PycharmProjects/RegisMaster')
     options = ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-automation'])
     options.add_argument('--ignore-certificate-errors')
@@ -145,4 +193,5 @@ def main_program():
 thread_001 = threading.Thread(target=os_mitmproxy)
 thread_002 = threading.Thread(target=main_program)
 thread_001.start()
+time.sleep(5)
 thread_002.start()
